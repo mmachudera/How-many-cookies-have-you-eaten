1eb5ad73fc7cb18b062b5254e598e98448c96379
machuderam <96541649+mmachudera@users.noreply.github.com>
2022-01-16 18:09:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Let's check if you can get another cookie.")
+quantity = int(input("How many cookies did you ate today? "))
+if quantity < 4:
+  print("Yes, please take one more.")
+else:
+  print("Sorry, it is enough for today.")
